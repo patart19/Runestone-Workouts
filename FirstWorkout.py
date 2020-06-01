93b488461d1b575e1316e8f468f5e67cfcc702c5
patart19 <62774304+patart19@users.noreply.github.com>
2020-06-01 19:54:09 -0400

    Starting adding some list methods.

--- FirstWorkout.py
+++ FirstWorkout.py
@@ -1,1 +1,40 @@
-# Test to see if I made this python file correctly.
+# List Methods
+
+y = [1, 2, 3, 4, 5]
+y.append(7)
+print(y)
+#adding the number 7 to the end of the list.
+
+y.reverse()
+print(y)
+
+#adding the number 6 to the 4th position in the list.
+
+y.insert(4, 6)
+print(y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

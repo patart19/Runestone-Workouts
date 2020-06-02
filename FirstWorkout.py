9f38c80b040a0171bce3188e03097d45d4b08764
patart19 <62774304+patart19@users.noreply.github.com>
2020-06-01 20:05:39 -0400

    Update FirstWorkout.py

--- FirstWorkout.py
+++ FirstWorkout.py
@@ -1,40 +1,26 @@
 # List Methods
 
+#adding the number 7 to the end of the list.
 y = [1, 2, 3, 4, 5]
 y.append(7)
 print(y)
-#adding the number 7 to the end of the list.
 
+#reverse the order of the list
 y.reverse()
 print(y)
 
-#adding the number 6 to the 4th position in the list.
+#putting the list back in order
+y.sort()
 
-y.insert(4, 6)
+#adding the number 6 to the 5th position in the list.
+y.insert(5, 6)
 print(y)
 
+#Create a list, myList, that includes 0 and 100 (non inclusive).
+myList = range(0,100)
+print(myList)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#Create a list, AnotherList, that includes 0 to 101 (non inclusive) in increments
+#of 20
+AnotherList = range(0, 101, 20)
+print(AnotherList)
